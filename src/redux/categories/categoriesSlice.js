9c7e37d118f11c5b29c5e1bab54acbbec279c318
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-04 20:43:43 +0000

    Enhance categorySlice file

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,20 +2,23 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
+  status: 'Under construction',
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
-
   reducers: {
-    checkStatus: (state, action) => {
-      state.categories = action.payload === 'Under construction'
-        ? 'Under construction'
-        : state.categories;
+    getCategoryStatus: (state, action) => {
+      // Check if the category exists in the state
+      const { categoryId } = action.payload;
+      const category = state.categories.find((cat) => cat.id === categoryId);
+
+      // If the category exists, return its status; otherwise, return "Under construction"
+      state.status = category ? category.status : 'Under construction';
     },
   },
 });
 
-export const { checkStatus } = categoriesSlice.actions;
+export const { getCategoryStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
