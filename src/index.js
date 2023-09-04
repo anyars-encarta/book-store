2e47321b007b91f9200b2609f9b5fcac3c90acde
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-04 19:12:51 +0000

    Create slices for books and categories

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
