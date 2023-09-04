6cfff3ac9eed041f7df97c54f372914217cc2b5e
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-04 20:35:42 +0000

    Effect CR requested changes

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,13 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
-  status: 'Under construction',
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
-  reducers: {},
+
+  reducers: {
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
+  },
 });
 
+export const { checkStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
+
+const rootReducer = {
+  books: booksReducer,
+  categories: categoriesReducer,
+};
 
 const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
 });
 
 export default store;
